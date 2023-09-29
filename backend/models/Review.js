7843a40dc34290e33f612e3823bc06e714789277
Kacper Stępień <kacper2007x48@gmail.com>
2023-09-29 11:53:45 +0200

    refactor: remove userHasPurchasedProduct middleware and implement purchase validation in Review model in pre-save hook instead

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Product = require("./Product");
 const User = require("./User");
+const Order = require("./Order");
 
 const reviewSchema = new mongoose.Schema({
   product: {
@@ -28,15 +29,28 @@ const reviewSchema = new mongoose.Schema({
 reviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
 reviewSchema.pre("save", async function (next) {
+  // Check if product exists
   const product = await Product.findById(this.product);
   if (!product) {
     return next(new Error("Produkt nie istnieje"));
   }
 
+  // Check if user has purchased product
   const user = await User.findById(this.user);
   if (!user) {
     return next(new Error("Użytkownik nie istnieje"));
   }
+
+  // Check if user has purchased product
+  const order = await Order.findOne({
+    customer: user._id,
+    "items.product": product._id,
+  });
+
+  if (!order) {
+    return next(new Error("Aby ocenić produkt musisz go najpierw kupić"));
+  }
+
   next();
 });
 

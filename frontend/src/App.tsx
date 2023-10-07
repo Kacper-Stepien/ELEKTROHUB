17575db274dad22ca24d2da78bbd212d72547e11
Kacper Stępień <kacper2007x48@gmail.com>
2023-10-07 13:27:00 +0200

    implement theme toggling with Redux Toolkit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
+import { useAppDispatch, useAppSelector } from "./store/store";
+
+import { setTheme } from "./store/features/themeSlice";
+
 function App() {
+  const theme = useAppSelector((state) => state.theme.theme);
+
+  const dispatch = useAppDispatch();
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      dispatch(setTheme({ theme: "dark" }));
+    } else {
+      dispatch(setTheme({ theme: "light" }));
+    }
+  };
+
   return (
-    <div>
+    <div className="h-screen bg-blue-50 bg-pizza dark:bg-primaryDark">
       <p>Sklep</p>
+      <button onClick={toggleTheme}>Toggle theme</button>
     </div>
   );
 }
---
+++ frontend/src/store/features/themeSlice.ts
@@ -0,0 +1,32 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+export interface ThemeState {
+  theme: string;
+}
+
+const initialState: ThemeState = {
+  theme: localStorage.getItem("ElektroHub:theme") || "dark",
+};
+
+if (initialState.theme === "dark") {
+  document.documentElement.classList.add("dark");
+}
+
+const ThemeSlice = createSlice({
+  name: "theme",
+  initialState,
+  reducers: {
+    setTheme(state, action: PayloadAction<{ theme: string }>) {
+      state.theme = action.payload.theme;
+      localStorage.setItem("ElektroHub:theme", action.payload.theme);
+      if (action.payload.theme === "dark") {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+    },
+  },
+});
+
+export const { setTheme } = ThemeSlice.actions;
+export default ThemeSlice.reducer;
